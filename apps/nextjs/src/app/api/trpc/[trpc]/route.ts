9b6184d9461930e3eaf67b52e5683a6d96f62004
maxwiseman <max@maxwiseman.io>
2024-12-14 00:42:03 -0600

    chore: reenabled edge runtime

--- apps/nextjs/src/app/api/trpc/[trpc]/route.ts
+++ apps/nextjs/src/app/api/trpc/[trpc]/route.ts
@@ -3,6 +3,8 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter, createTRPCContext } from "@scibo/api";
 import { auth } from "@scibo/auth";
 
+export const runtime = "edge";
+
 /**
  * Configure basic CORS headers
  * You should extend this to match your needs
